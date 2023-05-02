2c84bfd24ee1fbca4c1d112a572fc121f54b9778
sourav <souravmahanty12@gmail.com>
2023-05-02 19:02:30 +0530

    new changes done

--- src/App.js
+++ src/App.js
@@ -1,32 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './components/header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./components/header";
 import React from "react";
-import WeatherForm from './components/weather_form';
-import WeatherCard from './components/weather_card';
+import WeatherForm from "./components/weather_form";
+import WeatherCard from "./components/weather_card";
 import Map from "./components/map";
 
 function App() {
-
   const [weatherData, setWeatherData] = React.useState([]);
   const [coord, setCoord] = React.useState({
     lng: 0,
-    lat: 0
+    lat: 0,
   });
 
   const handleCoord = (newCoord) => {
-    setCoord(newCoord)
-  }
-  
+    console.log("handlecoord");
+    setCoord(newCoord);
+  };
+
   const handleWeatherData = (newData) => {
-    setWeatherData(newData)
-  }
+    setWeatherData(newData);
+  };
 
   return (
     <div className="App">
       <Header />
       <Map coord={coord} />
-      <WeatherForm handleWeatherData={handleWeatherData} handleCoordData={handleCoord} />
+      <WeatherForm
+        handleWeatherData={handleWeatherData}
+        handleCoordData={handleCoord}
+      />
       <WeatherCard weatherData={weatherData} />
     </div>
   );
--- src/components/map.js
+++ src/components/map.js
@@ -1,20 +1,24 @@
 import GoogleMapReact from "google-map-react";
+// import { GoogleMap, InfoBox, Marker } from "@react-google-maps/api";
 import React from "react";
 
 const Map = (props) => {
   const [zoom, setZoom] = React.useState(11);
-    return (
-        <div style={{ height: "500px", width: "100%" }}>
-          <GoogleMapReact
-            bootstrapURLKeys={{ key: "AIzaSyBb_903sk414N5BTdsDWXowkw9B-SjsVtA" }}
-            defaultCenter={props.coord}
-            defaultZoom={zoom}
-          >
-            <Marker lat={props.coord.lat} lng={props.coord.lng} />
-          </GoogleMapReact>
-        </div>
-      );
-}
+  console.log(props.coord);
+  return (
+    <div style={{ height: "500px", width: "100%" }}>
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: "AIzaSyBb_903sk414N5BTdsDWXowkw9B-SjsVtA" }}
+        defaultCenter={props.coord}
+        defaultZoom={zoom}
+      >
+        <Marker lat={props.coord.lat} lng={props.coord.lng} />
+        {/* <InfoBox position={{lat:props.lat , lng:props.lng}} ><h1>{props.temp}</h1></InfoBox>
+        <Marker position={{lat:props.lat , lng:props.lng} } ></Marker> */}
+      </GoogleMapReact>
+    </div>
+  );
+};
 
 const Marker = () => <div style={{ color: "red", fontSize: "25px" }}>📍</div>;
 
